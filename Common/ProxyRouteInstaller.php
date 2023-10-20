9917fddcd2c78a6f9042940d5bb440caf56c5edb
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2023-10-20 14:08:43 +0200

    NEW MetaModelAdditionInstaller can be used to include master data in app

--- Common/ProxyRouteInstaller.php
+++ Common/ProxyRouteInstaller.php
@@ -4,6 +4,7 @@ namespace axenox\Proxy\Common;
 use exface\Core\CommonLogic\AppInstallers\MetaModelAdditionInstaller;
 use exface\Core\Interfaces\Selectors\SelectorInterface;
 use exface\Core\Interfaces\InstallerContainerInterface;
+use exface\Core\CommonLogic\AppInstallers\AbstractAppInstaller;
 
 /**
  * Allows to add proxy routes to app packages
@@ -19,8 +20,10 @@ use exface\Core\Interfaces\InstallerContainerInterface;
  * @author Andrej Kabachnik
  *
  */
-class ProxyRouteInstaller extends MetaModelAdditionInstaller
+class ProxyRouteInstaller extends AbstractAppInstaller
 {
+    private $additionInstaller = null;
+    
     /**
      *
      * @param SelectorInterface $selectorToInstall
@@ -28,8 +31,37 @@ class ProxyRouteInstaller extends MetaModelAdditionInstaller
      */
     public function __construct(SelectorInterface $selectorToInstall, InstallerContainerInterface $installerContainer)
     {
-        parent::__construct($selectorToInstall, $installerContainer);
-        $modelFolder = 'Proxy';
-        $this->addModelDataSheet($modelFolder, $this->createModelDataSheet('axenox.Proxy.proxy_route', 'APP', 'MODIFIED_ON'));
+        $this->additionInstaller = (new MetaModelAdditionInstaller($selectorToInstall, $installerContainer, 'Proxy'))
+        ->addDataToReplace('axenox.Proxy.proxy_route', 'MODIFIED_ON', 'APP');
+    }
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\Core\Interfaces\InstallerInterface::backup()
+     */
+    public function backup(string $absolute_path): \Iterator
+    {
+        return $this->additionInstaller->backup($absolute_path);
+    }
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\Core\Interfaces\InstallerInterface::uninstall()
+     */
+    public function uninstall(): \Iterator
+    {
+        return $this->additionInstaller->uninstall();
+    }
+    
+    /**
+     *
+     * {@inheritDoc}
+     * @see \exface\Core\Interfaces\InstallerInterface::install()
+     */
+    public function install(string $source_absolute_path): \Iterator
+    {
+        return $this->additionInstaller->install($source_absolute_path);
     }
 }
